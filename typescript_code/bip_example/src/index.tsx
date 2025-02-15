9039e680da4b74e42bd8d6bfa492e706a7f93592
19年梦醒 <3949379+getong@users.noreply.github.com>
2025-02-16 02:55:29 +0800

    add doc

--- typescript_code/bip_example/src/index.tsx
+++ typescript_code/bip_example/src/index.tsx
@@ -2,3 +2,5 @@ import * as bip39 from "bip39";
 
 const mnemonic = bip39.generateMnemonic();
 console.log("mnemonic is: \n" + mnemonic);
+
+// copy from https://solana.com/zh/developers/cookbook/wallets/generate-mnemonic
