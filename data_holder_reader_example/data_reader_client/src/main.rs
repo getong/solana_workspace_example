89477516a0277ac7895ab49966a65696c9319e62
19年梦醒 <3949379+getong@users.noreply.github.com>
2025-07-26 15:59:49 +0800

    add data_reader client example

--- data_holder_reader_example/data_reader_client/src/main.rs
+++ data_holder_reader_example/data_reader_client/src/main.rs
@@ -1,6 +1,13 @@
 use std::{rc::Rc, str::FromStr};
 
-use anchor_client::{Client, Cluster, solana_sdk::pubkey::Pubkey};
+use anchor_client::{
+  solana_sdk::{
+    instruction::{AccountMeta, Instruction},
+    pubkey::Pubkey,
+  },
+  Client, Cluster,
+};
+use serde_json::Value;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
   // Your wallet keypair (used to sign tx)
@@ -12,6 +19,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
   // Program ID of your data-reader program
   let program_id = Pubkey::from_str("59cnY1rjumvuLbab9MRqFmk62QALDmjFRBzMEupqeyzH")?;
 
+  // Load IDL from JSON file
+  let idl_str = include_str!("../idl/data_reader.json");
+  let idl: Value = serde_json::from_str(idl_str)?;
+
+  // Find the read_other_data instruction discriminator from IDL
+  let instructions = idl["instructions"].as_array().unwrap();
+  let read_other_data_ix = instructions
+    .iter()
+    .find(|ix| ix["name"] == "read_other_data") // Changed from "readOtherData" to "read_other_data"
+    .expect("read_other_data instruction not found in IDL");
+
+  // Get the discriminator (first 8 bytes)
+  let discriminator = read_other_data_ix["discriminator"]
+    .as_array()
+    .unwrap()
+    .iter()
+    .map(|v| v.as_u64().unwrap() as u8)
+    .collect::<Vec<u8>>();
+
   // Create client and program instance
   let client = Client::new(Cluster::Localnet, payer.clone());
   let program = client.program(program_id)?;
@@ -19,16 +45,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
   // The other account address to pass (same as in TypeScript)
   let other_storage_pubkey = Pubkey::from_str("HwUtrTmLy6aHdDWSNN38rpRZy6icDhyCiVYD7XHV3LZF")?;
 
-  // Build the transaction using proper Anchor client pattern
-  let tx = program
-    .request()
-    .accounts(vec![
-      anchor_client::solana_sdk::instruction::AccountMeta::new_readonly(
-        other_storage_pubkey,
-        false,
-      ),
-    ])
-    .send()?;
+  // Build the instruction manually using IDL data
+  let ix = Instruction {
+    program_id,
+    accounts: vec![AccountMeta::new_readonly(other_storage_pubkey, false)],
+    data: discriminator, // Use discriminator from IDL
+  };
+
+  // Send the transaction
+  let tx = program.request().instruction(ix).send()?;
 
   println!("Transaction signature: {:?}", tx);
 
